6f93e45f0a2499f2cda48d664dc2acae9d668f45
Ruqeert <jakub.rupnicki@gmail.com>
2019-10-26 14:44:39 +0200

    Poprawa packegu JR

--- src/test/SuperCarJR.java
+++ src/test/SuperCarJR.java
@@ -5,10 +5,6 @@ public class SuperCarJR {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
-		
-		System.out.println();
-		
 		DaneJR Ferrari = new DaneJR("Ferrari", "559", "red", 4.5);
 		Ferrari.showCarData();
 		System.out.println(Ferrari.toString());
