42718141f4a1c00fa7bb39b54055f6b045529fcc
unknown <admin@EDU-WRF-L05.tb.hq2>
2019-10-26 15:28:16 +0200

    Added Cat race

---
+++ src/test/MainCat.java
@@ -0,0 +1,17 @@
+package test;
+
+public class MainCat {
+
+	public static void main(String[] args) {
+		Cat Catmruczek = new Cat();
+			
+		Catmruczek.name = "mruczek";
+		Catmruczek.breed = "ragdoll";
+		Catmruczek.color = "white";
+		Catmruczek.age = 4;
+		Catmruczek.showCatData();
+		
+
+	}
+
+}
