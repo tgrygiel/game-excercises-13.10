4563745d624f27d88c2ad0b638bf97049c377c69
Constantine Gavura <kostya.gavura@gmail.com>
2019-10-26 13:23:35 +0200

    Added package kostyagavura and created object DogKG

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,36 @@
 import test.TestTomekG;
 
 public class Main {
-	
+
     public static void main(final String[] args) {
         final Player tomek = new Player("Tomek", true);
         final Player kamila = new Player("Kamila", false);
-        
+
         //Sparametryzuj wielkosc tablicy
         Board board = new Board();
         System.out.println(board);
-        
+
         tomek.makeAMove(1, 2, 1, board);
         kamila.makeAMove(2, 2, 0, board);
         System.out.println(board);
-        
-        
-        
+
+
+
         System.out.println(tomek);
         System.out.println(kamila);
-        
-        
+
+
         //test SYSO
         System.out.println();
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
+
         // Tests classes
         TestTomekG testObjectTomeksClass =  new TestTomekG("Tomek");
         System.out.println(testObjectTomeksClass);
